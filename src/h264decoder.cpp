b37e18d8005ad60ec31b583933e90f5d68bb0491
lukelook <lukeluocn@outlook.com>
2021-07-25 11:10:35 +0800

    Format C++ sources

--- src/h264decoder.cpp
+++ src/h264decoder.cpp
@@ -1,4 +1,5 @@
-extern "C" {
+extern "C"
+{
 #include <libavcodec/avcodec.h>
 #include <libavutil/avutil.h>
 #include <libavutil/imgutils.h>
@@ -11,136 +12,132 @@ extern "C" {
 typedef unsigned char ubyte;
 
 /* For backward compatibility with release 9 or so of libav */
-#if (LIBAVCODEC_VERSION_MAJOR <= 54) 
-#  define av_frame_alloc avcodec_alloc_frame
-#  define av_frame_free  avcodec_free_frame  
+#if (LIBAVCODEC_VERSION_MAJOR <= 54)
+#define av_frame_alloc avcodec_alloc_frame
+#define av_frame_free avcodec_free_frame
 #endif
-
 
 H264Decoder::H264Decoder()
 {
-  avcodec_register_all();
+    avcodec_register_all();
 
-  codec = avcodec_find_decoder(AV_CODEC_ID_H264);
-  if (!codec)
-    throw H264InitFailure("cannot find decoder");
-  
-  context = avcodec_alloc_context3(codec);
-  if (!context)
-    throw H264InitFailure("cannot allocate context");
+    codec = avcodec_find_decoder(AV_CODEC_ID_H264);
+    if (!codec)
+        throw H264InitFailure("cannot find decoder");
 
-  // Note: CODEC_CAP_TRUNCATED was prefixed with AV_...
-  if(codec->capabilities & AV_CODEC_CAP_TRUNCATED) {
-    context->flags |= AV_CODEC_FLAG_TRUNCATED;
-  }  
+    context = avcodec_alloc_context3(codec);
+    if (!context)
+        throw H264InitFailure("cannot allocate context");
 
-  int err = avcodec_open2(context, codec, nullptr);
-  if (err < 0)
-    throw H264InitFailure("cannot open context");
+    // Note: CODEC_CAP_TRUNCATED was prefixed with AV_...
+    if (codec->capabilities & AV_CODEC_CAP_TRUNCATED)
+    {
+        context->flags |= AV_CODEC_FLAG_TRUNCATED;
+    }
 
-  parser = av_parser_init(AV_CODEC_ID_H264);
-  if (!parser)
-    throw H264InitFailure("cannot init parser");
-  
-  frame = av_frame_alloc();
-  if (!frame)
-    throw H264InitFailure("cannot allocate frame");
+    int err = avcodec_open2(context, codec, nullptr);
+    if (err < 0)
+        throw H264InitFailure("cannot open context");
+
+    parser = av_parser_init(AV_CODEC_ID_H264);
+    if (!parser)
+        throw H264InitFailure("cannot init parser");
+
+    frame = av_frame_alloc();
+    if (!frame)
+        throw H264InitFailure("cannot allocate frame");
 
 #if 1
-  pkt = new AVPacket;
-  if (!pkt)
-    throw H264InitFailure("cannot allocate packet");
-  av_init_packet(pkt);
+    pkt = new AVPacket;
+    if (!pkt)
+        throw H264InitFailure("cannot allocate packet");
+    av_init_packet(pkt);
 #endif
 }
-
 
 H264Decoder::~H264Decoder()
 {
-  av_parser_close(parser);
-  avcodec_close(context);
-  av_free(context);
-  av_frame_free(&frame);
+    av_parser_close(parser);
+    avcodec_close(context);
+    av_free(context);
+    av_frame_free(&frame);
 #if 1
-  delete pkt;
+    delete pkt;
 #endif
 }
 
-
-ptrdiff_t H264Decoder::parse(const ubyte* in_data, ptrdiff_t in_size)
+ptrdiff_t H264Decoder::parse(const ubyte *in_data, ptrdiff_t in_size)
 {
-  auto nread = av_parser_parse2(parser, context, &pkt->data, &pkt->size, 
-                                in_data, in_size, 
-                                0, 0, AV_NOPTS_VALUE);
-  return nread;
+    auto nread = av_parser_parse2(parser, context, &pkt->data, &pkt->size,
+                                  in_data, in_size,
+                                  0, 0, AV_NOPTS_VALUE);
+    return nread;
 }
-
 
 bool H264Decoder::is_frame_available() const
 {
-  return pkt->size > 0;
+    return pkt->size > 0;
 }
 
-
-const AVFrame& H264Decoder::decode_frame()
+const AVFrame &H264Decoder::decode_frame()
 {
 #if (LIBAVCODEC_VERSION_MAJOR > 56)
-  int ret;
-  if (pkt) {
-    ret = avcodec_send_packet(context, pkt);
-    if (!ret) {
-      ret = avcodec_receive_frame(context, frame);
-      if (!ret)
-        return *frame;
+    int ret;
+    if (pkt)
+    {
+        ret = avcodec_send_packet(context, pkt);
+        if (!ret)
+        {
+            ret = avcodec_receive_frame(context, frame);
+            if (!ret)
+                return *frame;
+        }
     }
-  }
-  throw H264DecodeFailure("error decoding frame");
+    throw H264DecodeFailure("error decoding frame");
 #else
-  int got_picture = 0;
-  int nread = avcodec_decode_video2(context, frame, &got_picture, pkt);
-  if (nread < 0 || got_picture == 0)
-    throw H264DecodeFailure("error decoding frame\n");
-  return *frame;
+    int got_picture = 0;
+    int nread = avcodec_decode_video2(context, frame, &got_picture, pkt);
+    if (nread < 0 || got_picture == 0)
+        throw H264DecodeFailure("error decoding frame\n");
+    return *frame;
 #endif
 }
 
-
 ConverterRGB24::ConverterRGB24()
 {
-  framergb = av_frame_alloc();
-  if (!framergb)
-    throw H264DecodeFailure("cannot allocate frame");
-  context = nullptr;
+    framergb = av_frame_alloc();
+    if (!framergb)
+        throw H264DecodeFailure("cannot allocate frame");
+    context = nullptr;
 }
 
 ConverterRGB24::~ConverterRGB24()
 {
-  sws_freeContext(context);
-  av_frame_free(&framergb);
+    sws_freeContext(context);
+    av_frame_free(&framergb);
 }
 
-
-const AVFrame& ConverterRGB24::convert(const AVFrame &frame, ubyte* out_rgb)
+const AVFrame &ConverterRGB24::convert(const AVFrame &frame, ubyte *out_rgb)
 {
-  int w = frame.width;
-  int h = frame.height;
-  int pix_fmt = frame.format;
-  
-  context = sws_getCachedContext(context, 
-                                 w, h, (AVPixelFormat)pix_fmt, 
-                                 w, h, AV_PIX_FMT_RGB24, SWS_BILINEAR,
-                                 nullptr, nullptr, nullptr);
-  if (!context)
-    throw H264DecodeFailure("cannot allocate context");
-  
-  // Setup framergb with out_rgb as external buffer. Also say that we want RGB24 output.
-  av_image_fill_arrays(framergb->data, framergb->linesize, out_rgb, AV_PIX_FMT_RGB24, w, h, 1);
-  // Do the conversion.
-  sws_scale(context, frame.data, frame.linesize, 0, h,
-            framergb->data, framergb->linesize);
-  framergb->width = w;
-  framergb->height = h;
-  return *framergb;
+    int w = frame.width;
+    int h = frame.height;
+    int pix_fmt = frame.format;
+
+    context = sws_getCachedContext(context,
+                                   w, h, (AVPixelFormat)pix_fmt,
+                                   w, h, AV_PIX_FMT_RGB24, SWS_BILINEAR,
+                                   nullptr, nullptr, nullptr);
+    if (!context)
+        throw H264DecodeFailure("cannot allocate context");
+
+    // Setup framergb with out_rgb as external buffer. Also say that we want RGB24 output.
+    av_image_fill_arrays(framergb->data, framergb->linesize, out_rgb, AV_PIX_FMT_RGB24, w, h, 1);
+    // Do the conversion.
+    sws_scale(context, frame.data, frame.linesize, 0, h,
+              framergb->data, framergb->linesize);
+    framergb->width = w;
+    framergb->height = h;
+    return *framergb;
 }
 
 /*
@@ -153,23 +150,20 @@ fill the buffer we should also use it to determine the required size.
 */
 int ConverterRGB24::predict_size(int w, int h)
 {
-  return av_image_fill_arrays(framergb->data, framergb->linesize, nullptr, AV_PIX_FMT_RGB24, w, h, 1);
+    return av_image_fill_arrays(framergb->data, framergb->linesize, nullptr, AV_PIX_FMT_RGB24, w, h, 1);
 }
 
-
-
-std::pair<int, int> width_height(const AVFrame& f)
+std::pair<int, int> width_height(const AVFrame &f)
 {
-  return std::make_pair(f.width, f.height);
+    return std::make_pair(f.width, f.height);
 }
 
-int row_size(const AVFrame& f)
+int row_size(const AVFrame &f)
 {
-  return f.linesize[0];
+    return f.linesize[0];
 }
-
 
 void disable_logging()
 {
-  av_log_set_level(AV_LOG_QUIET);
+    av_log_set_level(AV_LOG_QUIET);
 }
--- src/h264decoder.hpp
+++ src/h264decoder.hpp
@@ -28,78 +28,77 @@ struct AVCodecParserContext;
 struct SwsContext;
 struct AVPacket;
 
-
 class H264Exception : public std::runtime_error
 {
 public:
-  H264Exception(const char* s) : std::runtime_error(s) {}
+    H264Exception(const char *s) : std::runtime_error(s) {}
 };
 
 class H264InitFailure : public H264Exception
 {
 public:
-    H264InitFailure(const char* s) : H264Exception(s) {}
+    H264InitFailure(const char *s) : H264Exception(s) {}
 };
 
 class H264DecodeFailure : public H264Exception
 {
 public:
-    H264DecodeFailure(const char* s) : H264Exception(s) {}
+    H264DecodeFailure(const char *s) : H264Exception(s) {}
 };
-
 
 class H264Decoder
 {
-  /* Persistent things here, using RAII for cleanup. */
-  AVCodecContext        *context;
-  AVFrame               *frame;
-  AVCodec               *codec;
-  AVCodecParserContext  *parser;
-  /* In the documentation example on the github master branch, the 
+    /* Persistent things here, using RAII for cleanup. */
+    AVCodecContext *context;
+    AVFrame *frame;
+    AVCodec *codec;
+    AVCodecParserContext *parser;
+    /* In the documentation example on the github master branch, the 
 packet is put on the heap. This is done here to store the pointers 
 to the encoded data, which must be kept around  between calls to 
 parse- and decode frame. In release 11 it is put on the stack, too. 
   */
-  AVPacket              *pkt;
+    AVPacket *pkt;
+
 public:
-  H264Decoder();
-  ~H264Decoder();
-  /* First, parse a continuous data stream, dividing it into 
+    H264Decoder();
+    ~H264Decoder();
+    /* First, parse a continuous data stream, dividing it into 
 packets. When there is enough data to form a new frame, decode 
 the data and return the frame. parse returns the number 
 of consumed bytes of the input stream. It stops consuming 
 bytes at frame boundaries.
   */
-  ptrdiff_t parse(const unsigned char* in_data, ptrdiff_t in_size);
-  bool is_frame_available() const;
-  const AVFrame& decode_frame();
+    ptrdiff_t parse(const unsigned char *in_data, ptrdiff_t in_size);
+    bool is_frame_available() const;
+    const AVFrame &decode_frame();
 };
 
 // TODO: Rename to OutputStage or so?!
 class ConverterRGB24
 {
-  SwsContext *context;
-  AVFrame *framergb;
-  
+    SwsContext *context;
+    AVFrame *framergb;
+
 public:
-  ConverterRGB24();
-  ~ConverterRGB24();
-   
-  /*  Returns, given a width and height, 
+    ConverterRGB24();
+    ~ConverterRGB24();
+
+    /*  Returns, given a width and height, 
       how many bytes the frame buffer is going to need. */
-  int predict_size(int w, int h);
-  /*  Given a decoded frame, convert it to RGB format and fill 
+    int predict_size(int w, int h);
+    /*  Given a decoded frame, convert it to RGB format and fill 
 out_rgb with the result. Returns a AVFrame structure holding 
 additional information about the RGB frame, such as the number of
 bytes in a row and so on. */
-  const AVFrame& convert(const AVFrame &frame, unsigned char* out_rgb);
+    const AVFrame &convert(const AVFrame &frame, unsigned char *out_rgb);
 };
 
 void disable_logging();
 
 /* Wrappers, so we don't have to include libav headers. */
-std::pair<int, int> width_height(const AVFrame&);
-int row_size(const AVFrame&);
+std::pair<int, int> width_height(const AVFrame &);
+int row_size(const AVFrame &);
 
 /* all the documentation links
  * My version of libav on ubuntu 16 appears to be from the release/11 branch on github
