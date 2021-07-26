a68310f767389e79d144fa56eba4a14796302833
lukelook <lukeluocn@outlook.com>
2021-07-26 17:52:04 +0800

    Add gop test

---
+++ src/decode/test_extract.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "h264decoder.h"
+
+
+const size_t BUFFER_SIZE = 1024;
+
+#define ERR_BUF_SIZE 1024
+char err_buf[ERR_BUF_SIZE] = {0};
+
+int main(int argc, char* argv[]) {
+    if (argc != 2 && argc != 3) {
+        printf("usage: {program_name} {path_to_h264_file} [no_output]\n");
+        return -1;
+    }
+
+    int no_output = 0;
+    if (argc == 3) {
+        no_output = 1;
+    }
+
+    // Allocate buffer
+    char buffer[BUFFER_SIZE];
+
+    // Open file
+    const char* v_filename = argv[1];
+    FILE* v_fp = fopen(v_filename, "rb");
+    if (!v_fp) {
+        printf("failed opening file %s\n", v_filename);
+        return -1;
+    }
+
+    // Create decoder
+    h264decoder decoder;
+    h264decoder* decoder_ptr = &decoder;
+
+    // Initialize decoder
+    int code;
+    const char* hint = h264decoder_init(decoder_ptr, &code);
+    if (code != 0) {
+        printf("failed initializing the decoder: %s\n", hint);
+        fclose(v_fp);
+        return -1;
+    }
+
+    // Decode video
+    size_t nread, nparsed;
+    size_t nframes = 0;
+    int is_keyframe = 0;
+    while (1) {
+        nread = fread(buffer, 1, BUFFER_SIZE, v_fp);
+        if (nread == 0) {
+            break;
+        }
+
+        const char* data = &buffer[0];
+        while (nread > 0) {
+            nparsed = h264decoder_parse(decoder_ptr, data, nread);
+            nread -= nparsed;
+            data += nparsed;
+            printf("nparsed: %d\n", nparsed);
+            if (h264decoder_available(decoder_ptr)) {
+                h264decoder_decode(decoder_ptr, &is_keyframe);
+
+                printf("frame format: %d\n", decoder_ptr->frame->format);
+                printf("frame width: %d\n", decoder_ptr->frame->width);
+                printf("frame height: %d\n", decoder_ptr->frame->height);
+
+                if (!no_output) {
+                    uint8_t* image = NULL;
+                    size_t image_size = 0;
+                    h264decoder_frame_to_jpeg(decoder_ptr, &image, &image_size);
+                    if (image_size) {
+                        char i_filename[20];
+                        sprintf(i_filename, "out%03d.jpg", nframes);
+                        FILE* i_fp = fopen(i_filename, "wb");
+                        fwrite(image, 1, image_size, i_fp);
+                        fclose(i_fp);
+                    }
+                }
+
+                printf("frame decoded: %d, keyframe: %d\n", nframes, is_keyframe);
+                nframes++;
+            }
+        }
+    };
+
+    {
+        int err = h264decoder_decode(decoder_ptr, &is_keyframe);
+        av_strerror(err, err_buf, ERR_BUF_SIZE);
+        printf("decode err: %s\n", err_buf);
+
+        decoder_ptr->frame->format = 0;
+        decoder_ptr->frame->width = 1280;
+        decoder_ptr->frame->height = 720;
+
+        if (!no_output) {
+            uint8_t* image = NULL;
+            size_t image_size = 0;
+            h264decoder_frame_to_jpeg(decoder_ptr, &image, &image_size);
+            if (image_size) {
+                char i_filename[20];
+                sprintf(i_filename, "out%03d.jpg", nframes);
+                FILE* i_fp = fopen(i_filename, "wb");
+                fwrite(image, 1, image_size, i_fp);
+                fclose(i_fp);
+            }
+        }
+
+        printf("frame decoded: %d, keyframe: %d\n", nframes, is_keyframe);
+        nframes++;
+    }
+
+    // Cleanup
+    h264decoder_free(decoder_ptr);
+    fclose(v_fp);
+    return 0;
+}
--- src/decode/test_gop.c
+++ src/decode/test_gop.c
@@ -3,18 +3,29 @@
 
 #include "h264decoder.h"
 
+#define BUFFER_SIZE 1024
+#define GOP_MAX_SIZE  2 * 1024 * 1024
 
-const size_t BUFFER_SIZE = 1024;
+char gop_buffer[GOP_MAX_SIZE];
 
-int main(int argc, char* argv[]) {
-    if (argc != 2 && argc != 3) {
-        printf("usage: {program_name} {path_to_h264_file} [no_output]\n");
-        return -1;
+void save_gop(size_t gop_len, int ngops) {
+    char gop_filename[20];
+    sprintf(gop_filename, "gop%03d.h264", ngops);
+
+    FILE* fp = fopen(gop_filename, "wb");
+    if (!fp) {
+        printf("failed opening file %s\n", gop_filename);
+        return;
     }
 
-    int no_output = 0;
-    if (argc == 3) {
-        no_output = 1;
+    fwrite(gop_buffer, 1, gop_len, fp);
+    fclose(fp);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("usage: {program_name} {path_to_h264_file}\n");
+        return -1;
     }
 
     // Allocate buffer
@@ -43,8 +54,15 @@ int main(int argc, char* argv[]) {
 
     // Decode video
     size_t nread, nparsed;
-    size_t nframes = 0;
+
+    size_t ngops = 0;
+    size_t gop_len = 0;
+    // Note that gop_buffer might contain the frame bytes of the next gop
+    size_t gop_buffer_len = 0;
+
     int is_keyframe = 0;
+    int first_keyframe = 1;
+
     while (1) {
         nread = fread(buffer, 1, BUFFER_SIZE, v_fp);
         if (nread == 0) {
@@ -54,29 +72,33 @@ int main(int argc, char* argv[]) {
         const char* data = &buffer[0];
         while (nread > 0) {
             nparsed = h264decoder_parse(decoder_ptr, data, nread);
+            memcpy(gop_buffer + gop_buffer_len, data, nparsed);
+            gop_buffer_len += nparsed;
             nread -= nparsed;
             data += nparsed;
+
+            // Frame is ready
             if (h264decoder_available(decoder_ptr)) {
                 h264decoder_decode(decoder_ptr, &is_keyframe);
-                nframes++;
-
-                if (!no_output) {
-                    uint8_t* image = NULL;
-                    size_t image_size = 0;
-                    h264decoder_frame_to_jpeg(decoder_ptr, &image, &image_size);
-                    if (image_size) {
-                        char i_filename[20];
-                        sprintf(i_filename, "out%03d.jpg", nframes);
-                        FILE* i_fp = fopen(i_filename, "wb");
-                        fwrite(image, 1, image_size, i_fp);
-                        fclose(i_fp);
+                if (is_keyframe) {
+                    if (first_keyframe) {
+                        first_keyframe = 0;
+                    } else {
+                        save_gop(gop_len, ngops);
+                        ngops++;
+                        gop_buffer_len -= gop_len;
+                        memcpy(gop_buffer, gop_buffer + gop_len, gop_buffer_len);
                     }
                 }
-
-                printf("frame decoded: %d, keyframe: %d\n", nframes, is_keyframe);
+                gop_len = gop_buffer_len;
             }
         }
     };
+
+    // Save the last gop
+    if (gop_buffer_len != 0) {
+        save_gop(gop_buffer_len, ngops);
+    }
 
     // Cleanup
     h264decoder_free(decoder_ptr);
